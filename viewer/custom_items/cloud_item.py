ffdc025645e8509c997edac07e44d6537e625a30
liu.yang <scomup@gmail.com>
2024-04-19 23:01:55 +0900

    draw points with intensity

--- viewer/custom_items/cloud_item.py
+++ viewer/custom_items/cloud_item.py
@@ -11,6 +11,73 @@ import time
 from PyQt5 import QtGui, QtCore
 
 
+vertex_shader = """
+#version 330 core
+
+layout (location = 0) in vec3 position;
+layout (location = 1) in float intensity;
+
+uniform mat4 view_matrix;
+uniform mat4 projection_matrix;
+uniform float alpha;
+out vec4 FragColor;
+
+vec3 getRainbowColor(float value) {
+    // Normalize value to [0, 1]
+    value = clamp(value, 0.0, 1.0);
+
+    // Convert value to hue in the range [0, 1]
+    float hue = value * 5.0 + 1.0;
+    int i = int(floor(hue));
+    float f = hue - float(i);
+    if (mod(i, 2) == 0) f = 1.0 - f; // if i is even
+    float n = 1.0 - f;
+
+    // Determine RGB components based on hue value
+    vec3 color;
+    if (i <= 1) color = vec3(n, 0.0, 1.0);
+    else if (i == 2) color = vec3(0.0, n, 1.0);
+    else if (i == 3) color = vec3(0.0, 1.0, n);
+    else if (i == 4) color = vec3(n, 1.0, 0.0);
+    else if (i >= 5) color = vec3(1.0, n, 0.0);
+
+    return color;
+}
+
+void main()
+{
+    vec4 pw = vec4(position, 1.0);
+    vec4 pc = view_matrix * pw;
+    gl_Position = projection_matrix * pc;
+    vec3 color = getRainbowColor(intensity);
+    FragColor = vec4(color, alpha);
+}
+"""
+
+fragment_shader = """
+#version 330 core
+
+in vec4 FragColor;
+
+out vec4 finalColor;
+
+void main()
+{
+    finalColor = FragColor;
+}
+"""
+
+def set_uniform_mat4(shader, content, name):
+    content = content.T
+    glUniformMatrix4fv(
+        glGetUniformLocation(shader, name),
+        1,
+        GL_FALSE,
+        content.astype(np.float32)
+    )
+
+
+# draw points with intensity (x, y, z, intensity)
 class CloudPlotItem(gl.GLGraphicsItem.GLGraphicsItem):
     def __init__(self, **kwds):
         super().__init__()
@@ -37,10 +104,6 @@ class CloudPlotItem(gl.GLGraphicsItem.GLGraphicsItem):
             pos = kwds.pop('pos')
             self.pos = np.ascontiguousarray(pos, dtype=np.float32)
             self.valid_point_num = pos.shape[0]
-        if 'color' in kwds:
-            self.color = kwds.pop('color')
-            self.color = np.ascontiguousarray(self.color, dtype=np.float32)
-            self.color[:, 3] = self.alpha
         self.need_init_buffer = True
 
     def updateRenderBuffer(self):
@@ -48,38 +111,54 @@ class CloudPlotItem(gl.GLGraphicsItem.GLGraphicsItem):
         if self.need_init_buffer:
             glBindBuffer(GL_ARRAY_BUFFER, self.vbo)
             glBufferData(GL_ARRAY_BUFFER, self.pos.nbytes, self.pos, GL_STATIC_DRAW)
-            # Create a color buffer object
-            glBindBuffer(GL_ARRAY_BUFFER, self.cbo)
-            glBufferData(GL_ARRAY_BUFFER, self.color.nbytes, self.color, GL_STATIC_DRAW)
+            self.valid_point_num = self.pos.shape[0]
+            glBindBuffer(GL_ARRAY_BUFFER, 0)
             self.need_init_buffer = False
         return
 
     def initializeGL(self):
+        self.program = shaders.compileProgram(
+            shaders.compileShader(vertex_shader, GL_VERTEX_SHADER),
+            shaders.compileShader(fragment_shader, GL_FRAGMENT_SHADER),
+        )
+        # Bind attribute locations
+        glUseProgram(self.program)
+        glBindAttribLocation(self.program, 0, "position")
+        glBindAttribLocation(self.program, 1, "intensity")
+        # set constant parameter for gaussian shader
+        project_matrix = np.array(self._GLGraphicsItem__view.projectionMatrix().data(), np.float32).reshape([4, 4]).T
+        set_uniform_mat4(self.program, project_matrix, 'projection_matrix')
+        glUniform1f(glGetUniformLocation(self.program, "alpha"), self.alpha)
         self.vbo = glGenBuffers(1)
-        self.cbo = glGenBuffers(1)
+        glUseProgram(0)
 
     def paint(self):
+        self.view_matrix = np.array(self._GLGraphicsItem__view.viewMatrix().data(), np.float32).reshape([4, 4]).T
         self.setupGLState()
         if self.valid_point_num == 0:
             return
-        self.updateRenderBuffer()
 
         glEnable(GL_BLEND)
         glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA)
 
-        glBindBuffer(GL_ARRAY_BUFFER, self.cbo)
-        glColorPointer(4, GL_FLOAT, 0, None)
-        glEnableClientState(GL_COLOR_ARRAY)
-
-        # draw points
-        glPointSize(self.size)
+        self.updateRenderBuffer()
+    
+        glUseProgram(self.program)
         glBindBuffer(GL_ARRAY_BUFFER, self.vbo)
-        glEnableClientState(GL_VERTEX_ARRAY)
-        glVertexPointer(3, GL_FLOAT, 0, None)
-        glDrawArrays(GL_POINTS, 0, self.valid_point_num)
-        glDisableClientState(GL_VERTEX_ARRAY)
-        glBindBuffer(GL_ARRAY_BUFFER, 0)
 
-        glDisableClientState(GL_COLOR_ARRAY)
+        set_uniform_mat4(self.program, self.view_matrix, 'view_matrix')
+        glEnableVertexAttribArray(0)
+        glEnableVertexAttribArray(1)        
+        glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 16, ctypes.c_void_p(0))
+        glVertexAttribPointer(1, 1, GL_FLOAT, GL_FALSE, 16, ctypes.c_void_p(12))
+        glDrawArrays(GL_POINTS, 0, len(self.pos))
+        
+        # Disable vertex attribute 
+        glDisableVertexAttribArray(0)
+        glDisableVertexAttribArray(1)        
+        # unbind VBO
         glBindBuffer(GL_ARRAY_BUFFER, 0)
+        # unbind VBO
+        glUseProgram(0)
+
 
