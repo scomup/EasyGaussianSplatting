621089c681ab693fe7cb1d7a3e1592ba34ece5d3
liu.yang <scomup@gmail.com>
2024-05-24 00:24:15 +0900

    full training

--- train.py
+++ train.py
@@ -2,22 +2,19 @@ import torch
 import numpy as np
 import torch.optim as optim
 import matplotlib.pyplot as plt
-import torchvision
 from gsplat.pytorch_ssim import gau_loss
 from gsplat.read_ply import *
 from gsplat.gausplat_dataset import *
 from gsnet import GSNet
-
+from random import randint
 import time
 
 
 if __name__ == "__main__":
     path = '/home/liu/bag/gaussian-splatting/tandt/train'
-    gs_dataset = GSplatDataset(path, device='cuda')
+    gs_set = GSplatDataset(path, device='cuda')
 
-    cam0, image0_gt = gs_dataset[0]
     gsnet = GSNet
-    gs = gs_dataset.gs
 
     ply_fn = "/home/liu/workspace/gaussian-splatting/output/train/point_cloud/iteration_10/point_cloud.ply"
     gs = load_ply(ply_fn)
@@ -31,26 +28,24 @@ if __name__ == "__main__":
     optimizer = optim.Adam(
         [rots, scales, shs, alphas, pws], lr=0.001, eps=1e-15)
 
+    cam0, _ = gs_set[0]
     fig, ax = plt.subplots()
     array = np.zeros(shape=(cam0.height, cam0.width, 3), dtype=np.uint8)
     im = ax.imshow(array)
 
-    for i in range(100):
-        # start = time.time()
-        image = gsnet.apply(rots, scales, shs, alphas, pws, cam0)
-        # image = image.requires_grad_()
-        loss = gau_loss(image, image0_gt)
-        # optimizer.zero_grad(set_to_none=True)
+    for iteration in range(0, 10000):
+        i = randint(0, len(gs_set) - 1)
+        cam, image_gt = gs_set[i]
+        image = gsnet.apply(rots, scales, shs, alphas, pws, cam)
+        loss = gau_loss(image, image_gt)
         loss.backward()
-        # end = time.time()
-        # time_diff = end - start
-        # print("runing %f\n" % time_diff)
         optimizer.step()
         optimizer.zero_grad(set_to_none=True)
-        if (i % 1 == 0):
-            print("step:%d loss:%f" % (i, loss.item()))
+        print("step:%d image: %d loss:%f" % (iteration, i, loss.item()))
+        if (i == 0):
+            print("step:%d loss:%f" % (iteration, loss.item()))
             im_cpu = image.to('cpu').detach().permute(1, 2, 0).numpy()
             im.set_data(im_cpu)
             fig.canvas.flush_events()
             plt.pause(0.1)
-    plt.show()
+        
